fc9a6488e2ea9066cd71274c00c1abf0dbbcd84c
AlexCastro <alexandre.castro@exakis-nelite.com>
2021-12-05 08:25:07 +0100

    no message

---
+++ ExtensionMethods.Sample/ExtensionMethods.Sample/ClassExtensioncs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods.Sample
+{
+    public static class ClassExtensioncs
+    {
+        public static string SayMyName(this Person p)
+        {
+            return  $"My name is: {p.FirstName} {p.LastName}";
+        }
+
+        public static string SayMyName(this PersonRecord p)
+        {
+            return $"My name is: {p.FirstName} {p.LastName}";
+        }
+    }
+}
---
+++ ExtensionMethods.Sample/ExtensionMethods.Sample/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExtensionMethods.Sample;
+
+//int extension
+var i = 10;
+var result = i.IsGreaterThan(100);
+
+Console.WriteLine(result);
+//person class extension
+var person = new Person { Age = 30, FirstName = "John", LastName = "Doe" };
+Console.WriteLine(person.SayMyName());
+
+//record extension 
+var personrecord = new PersonRecord("John", "Doe", 30);  
+Console.WriteLine(personrecord.SayMyName());
+
